3bab2da9a421e37c94923a66584801a9a2b8731f
Julio CN Souza <juliocnsouzadev@gmail.com>
2017-09-12 06:44:25 -0300

    class / encapsulation

--- Python-Crash-Course/oo/Profile.py
+++ Python-Crash-Course/oo/Profile.py
@@ -4,13 +4,18 @@
 # In[8]:
 
 
-class Profile():
+class Profile:
     def __init__(self, name, phone, company):
-        self.name = name
-        self.phone = phone
-        self.company =company
+        self.__name = name
+        self.__phone = phone
+        self.__company =company
+        self.__likes = 0
     
     def to_string(self):
-        return 'Name: ' + self.name + ', Phone: ' + self.phone + ', Company: ' + self.company
-        
-
+        return 'Name: ' + str(self.__name) + ', Phone: ' + str(self.__phone) + ', Company: ' + str(self.__company) + ', Likes: ' + str(self.__likes)
+    
+    def show_likes(self):
+        return self.__likes
+    
+    def like(self):
+        self.__likes += 1
