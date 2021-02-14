4d923dee5da4719cafd4b957f4a2d02a30c5bc1f
juliocnsouza <juliocnsouzadev@gmail.com>
2021-02-14 08:31:23 -0300

    self orgnizing maps

--- Python/email/sender.py
+++ Python/email/sender.py
@@ -6,13 +6,14 @@ username = "hungrypy@gmail.com"
 password = "iamhungry2016"
 from_email = username
 
+
 def sendmail(to_list, message):
     try:
         email_conn = SMTP(host, port)
         email_conn.ehlo()
         email_conn.starttls()
         email_conn.login(username, password)
-        email_conn.sendmail(from_email, to_list,  message)
+        email_conn.sendmail(from_email, to_list, message)
         email_conn.quit()
     except SMTPAuthenticationError:
         print("could not authenticate")
